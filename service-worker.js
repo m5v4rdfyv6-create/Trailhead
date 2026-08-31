1b12a77afa17962b64a68ee0c53efc7b374945c0
m5v4rdfyv6-create <m5v4rdfyv6@privaterelay.appleid.com>
2026-08-31 11:15:33 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='trailhead-collab-v7-cross-device-sync';
+const CACHE='trailhead-collab-v8-revision-sync';
 const ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./pine-bg.jpg','./desert-bg.jpg','./coast-bg.jpg','./alpine-bg.jpg','./autumn-bg.jpg','./night-bg.jpg'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
